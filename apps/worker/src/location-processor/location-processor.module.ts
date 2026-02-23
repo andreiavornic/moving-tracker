12987a6075750eeb86870013d3fb7e1608ab6a84
Avornic Andrei <andrei.av@mail.ru>
2026-02-23 20:55:48 +0200

    update mock

--- apps/worker/src/location-processor/location-processor.module.ts
+++ apps/worker/src/location-processor/location-processor.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { LocationProcessorService } from './location-processor.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LocationEntity } from 'libs/database/entities';
+import { BullModule } from '@nestjs/bullmq';
+import { LOCATION_QUEUE } from 'libs/common';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([LocationEntity])],
+  imports: [
+    TypeOrmModule.forFeature([LocationEntity]),
+    BullModule.registerQueue({ name: LOCATION_QUEUE }),
+  ],
   providers: [LocationProcessorService],
 })
 export class LocationProcessorModule {}
